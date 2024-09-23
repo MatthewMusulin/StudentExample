83e65993a895a8d3aaedc98a83057f3c31a1a42a
musulinm <musulinm@localhost>
2024-09-23 12:56:16 -0400

    Revert "reverted the boo"
    This reverts commit 0197a60a5959e3aa12e435d960b66f32e2d66e91.

---
+++ src/lecstudentexample/lecStudent.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lecstudentexample;
+
+/**
+ *
+ * @author musulinm
+ */
+public class lecStudent {
+    
+    private String name;
+    private int age;
+
+    public lecStudent(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+}
